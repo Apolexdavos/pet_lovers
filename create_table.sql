bf4e70afd53b239da55c23df74a5292fd7e234dd
Calmlycloudyfox <48609997+Calmlycloudyfox@users.noreply.github.com>
2020-08-04 12:29:30 +0700

    Update create_table.sql
    Tui có một số chỉnh sửa sau ông xme nha
    - add thêm bảng Service Record
    - xóa mục cost với service_id trong HealthRecord --> tách ra thành một bảng riêng để theo dõi chi phí luôn / và trong health thì sẽ k có gì liên quan đến chi phí cả
    - Thêm một số thông tin cho bảng owner
    - Chỉnh sửa thứ tự của một số cột
    - Đổi bảng Drug với Issue và liên hệ của nó với HealthRecord
    
    Cái datetime để dạng VARCHAR(15) hay là DATE á John?

--- create_table.sql
+++ create_table.sql
@@ -2,19 +2,27 @@
 -- CREATE DATABASE <database name>;
 -- USE <database name>;
 
-DROP TABLE IF EXISTS 
-    Action, 
+DROP TABLE IF EXISTS
+    Action,
     Disease, Drugs, HealthRecord,
     Appointment,
     CareGiver, Service, Provider,
     ServiceType, ProviderType,
-    Pet, Owner;
+    Pet, Owner, ServiceRecord;
 
 CREATE TABLE Owner
 (
   owner_id VARCHAR(15) NOT NULL,
   username VARCHAR(25) NOT NULL,
   password VARCHAR(256) NOT NULL,
+  YoB INT NOT NULL,
+  Address VARCHAR(25) NOT NULL,
+  Ward VARCHAR(25) NOT NULL,
+  District VARCHAR(25) NOT NULL,
+  City VARCHAR(25) NOT NULL,
+  email VARCHAR(25) NOT NULL,
+  phone VARCHAR(15) NOT NULL,
+  avatar DEFAULT "https://st.quantrimang.com/photos/image/072015/22/avatar.jpg"
   PRIMARY KEY (owner_id)
 );
 
@@ -36,19 +44,18 @@ CREATE TABLE ServiceType
 CREATE TABLE Provider
 (
   provide_id VARCHAR(15) NOT NULL,
+  type_id VARCHAR(15) NOT NULL,
   provide_name VARCHAR(25) NOT NULL,
   email VARCHAR(25),
   phone VARCHAR(15) NOT NULL,
   website VARCHAR(25),
-  address_nr VARCHAR(15) NOT NULL,
-  street VARCHAR(25) NOT NULL,
+  address_nr_street VARCHAR(25) NOT NULL,
   ward VARCHAR(15) NOT NULL,
   district VARCHAR(15) NOT NULL,
   city VARCHAR(15) NOT NULL,
   facebook VARCHAR(25),
   CN1 VARCHAR(25),
   CN2 VARCHAR(25),
-  type_id VARCHAR(15) NOT NULL,
   PRIMARY KEY (provide_id),
   FOREIGN KEY (type_id) REFERENCES ProviderType(type_id),
   UNIQUE (provide_name),
@@ -74,10 +81,12 @@ CREATE TABLE Appointment
   app_id VARCHAR(15) NOT NULL,
   app_date DATE NOT NULL,
   app_time TIME NOT NULL,
-  cost FLOAT NOT NULL,
-  owner_id VARCHAR(15) NOT NULL,
   provide_id VARCHAR(15) NOT NULL,
-  PRIMARY KEY (app_id, owner_id, provide_id),
+  service_id VARCHAR(15) NOT NULL,
+  owner_id VARCHAR(15) NOT NULL,
+  cost FLOAT NOT NULL,
+  PRIMARY KEY (app_id, service_id, owner_id, provide_id),
+  FOREIGN KEY (service_id) REFERENCES Service(service_id),
   FOREIGN KEY (owner_id) REFERENCES Owner(owner_id),
   FOREIGN KEY (provide_id) REFERENCES Provider(provide_id)
 );
@@ -107,16 +116,16 @@ CREATE TABLE Pet
 (
   pet_id VARCHAR(15) NOT NULL,
   pet_name VARCHAR(25) NOT NULL,
-  blood_type VARCHAR(5),
+  dob DATE,
   current_weight FLOAT NOT NULL,
-  current_age INT NOT NULL,
   gender VARCHAR(5) NOT NULL,
   status VARCHAR(10),
   type VARCHAR(25),
   breed VARCHAR(25),
   microchip VARCHAR(25),
   rabbies_tag VARCHAR(25),
-  image_dir VARCHAR(50) NOT NULL,
+  image_dir DEFAULT "https://images-na.ssl-images-amazon.com/images/I/61PZW9WMvBL._AC_SL1500_.jpg",
+  blood_type VARCHAR(5),
   reg_nr VARCHAR(25),
   reg_date DATE,
   insurance_id VARCHAR(25),
@@ -133,30 +142,46 @@ CREATE TABLE HealthRecord
 (
   datetime VARCHAR(15) NOT NULL,
   record_id VARCHAR(15) NOT NULL,
-  note VARCHAR(25),
-  illness VARCHAR(25) NOT NULL,
-  cost FLOAT NOT NULL,
-  cg_id VARCHAR(15) NOT NULL,
-  service_id VARCHAR(15) NOT NULL,
   pet_id VARCHAR(15) NOT NULL,
-  PRIMARY KEY (record_id, cg_id, service_id, pet_id),
+  cg_id VARCHAR(15) NOT NULL,
+  note VARCHAR(25),
+  issue_id VARCHAR(25) NOT NULL,
+  drug_id VARCHAR(25),
+  PRIMARY KEY (record_id, cg_id, pet_id),
+  FOREIGN KEY (drug_id) REFERENCES Drugs(drug_id),
   FOREIGN KEY (cg_id) REFERENCES CareGiver(cg_id),
   FOREIGN KEY (service_id) REFERENCES Service(service_id),
   FOREIGN KEY (pet_id) REFERENCES Pet(pet_id)
+  FOREIGN KEY (issue_id) REFERENCES Issue(issue_id)
 );
+CREATE TABLE ServiceRecord
+(
+  datetime VARCHAR(15) NOT NULL,
+  service_id VARCHAR(15) NOT NULL,
+  provide_id VARCHAR(15) NOT NULL,
+  cg_id VARCHAR(15),
+  owner_id VARCHAR(15) NOT NULL,
+  pet_id VARCHAR(15) NOT NULL,
+  cost FLOAT NOT NULL,
+  PRIMARY KEY (owner_id, provide_id, cg_id, service_id, pet_id),
+  FOREIGN KEY (provide_id) REFERENCES Provider(provide_id)
+  FOREIGN KEY (service_id) REFERENCES Service(service_id),
+  FOREIGN KEY (owner_id) REFERENCES Owner(owner_id),
+  FOREIGN KEY (cg_id) REFERENCES CareGiver(cg_id),
+  FOREIGN KEY (pet_id) REFERENCES Pet(pet_id)
+)
+
 
 CREATE TABLE Drugs
 (
+  drug_id VARCHAR(15) NOT NULL,
   drug_name VARCHAR(25) NOT NULL,
-  record_id VARCHAR(15) NOT NULL,
-  PRIMARY KEY (record_id),
-  FOREIGN KEY (record_id) REFERENCES HealthRecord(record_id)
+  PRIMARY KEY (drug_id),
 );
 
-CREATE TABLE Disease
+CREATE TABLE Issue
 (
-  disease_name VARCHAR(25) NOT NULL,
-  record_id VARCHAR(15) NOT NULL,
-  PRIMARY KEY (record_id),
-  FOREIGN KEY (record_id) REFERENCES HealthRecord(record_id)
+  issue_id VARCHAR(15) NOT NULL,
+  issue_name VARCHAR(25) NOT NULL,
+  PRIMARY KEY (issue_id),
 );
